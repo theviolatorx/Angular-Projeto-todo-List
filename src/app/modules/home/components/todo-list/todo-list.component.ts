17dce8d44be31ba6a3ccdfc0ba95f98c7c289312
Clovis Garcia <theviolatorx@gmail.com>
2023-10-13 00:32:57 -0300

    Feat: finish project

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -9,13 +9,13 @@ import { TaskList } from './../../model/task-list';
   styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements DoCheck{
-  public taskList: Array<TaskList> = [
-  ];
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem('list') || '[]');
 
 constructor(){}
 
 ngDoCheck(): void {
-    this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+  this.setLocalStorage();
+ 
 }
 
 public setEmitTaskList(event: string){
@@ -41,6 +41,13 @@ public setEmitTaskList(event: string){
       if(confirm){
         this.deleteItemTaskList(index);
       }
+    }
+  }
+
+  public setLocalStorage(){
+    if (this.taskList) {
+      this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+      localStorage.setItem('list', JSON.stringify(this.taskList));
     }
   }
 }
