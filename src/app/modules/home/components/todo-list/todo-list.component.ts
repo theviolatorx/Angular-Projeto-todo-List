522127eb7ed3063d84be064cb7791d1566afee8c
Clovis Garcia <theviolatorx@gmail.com>
2023-10-12 23:24:37 -0300

    Feat: delete task and all task

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+//Interface
+import { TaskList } from './../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent {
+  public taskList: Array<TaskList> = [
+    { task: 'teste', checked: false },
+    { task: 'teste', checked: true },
+  ];
 
+  public deleteItemTaskList(event: number): void {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList(): void {
+    const confirm: boolean = window.confirm('Você deseja Deletar Tudo?');
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
 }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -9,6 +9,7 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 // Page
 import { HomeComponent } from './pages/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -19,9 +20,11 @@ import { HomeComponent } from './pages/home/home.component';
     TodoInputAddItensComponent,
     TodoListComponent,
     HomeComponent
+    
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class HomeModule { }
