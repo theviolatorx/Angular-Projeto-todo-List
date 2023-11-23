21dc61da7649ffa86e9b055c25758170b2cfbd95
Clovis Garcia <theviolatorx@gmail.com>
2023-11-23 13:40:12 -0300

    Feat Botão logout

--- src/app/modules/home/components/header/header.component.ts
+++ src/app/modules/home/components/header/header.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  private isLogged:string = '';
+
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+      this.isLogged = JSON.stringify(localStorage.getItem("TOKEN"));
+  }
+
+  public logout(): void{
+    if (this.isLogged) {
+      localStorage.removeItem('TOKEN');
+      this.router.navigate(['/login']);
+    }
+  }
 }
